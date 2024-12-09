e782f12e1adb3844a9d504d68525e2a306910cf2
samuelsjolen <157499871+samuelsjolen@users.noreply.github.com>
2024-12-09 16:55:51 +0100

    Update rt_clk.vhdl
    Updated on lab computer

--- Real_time_clock/rt_clk.vhdl
+++ Real_time_clock/rt_clk.vhdl
@@ -10,8 +10,7 @@ entity rtc is
     sclk        : out     std_logic;  -- Pin 1 (Green)
     ce          : out     std_logic  -- Pin 3 (Blue)
     --init_byte_ver   : out     std_logic_vector(7 downto 0); -- VERIFICATION
-    --state       : out     std_logic_vector(2 downto 0); -- VERIFICATION
-    --data_recieved_ver : out std_logic_vector(7 downto 0)
+    --state       : out     std_logic_vector(2 downto 0) -- VERIFICATION
  );
   end entity;
 
@@ -27,20 +26,15 @@ entity rtc is
   signal init_byte      : std_logic_vector(7 downto 0);
   signal sclk_count     : std_logic;
   signal sclk_internal  : std_logic;
-  signal send           : std_logic;
-  signal data_recieved  : std_logic_vector(7 downto 0);
-  signal ce_internal    : std_logic;
-
+  --signal io             : std_logic; -- '0' indicates recieve, and vice versa
   
   signal current_state  : states;
   signal next_state     : states;
 
 
   begin
-    --init_byte_ver <= init_byte; -- Verifierar att 
+    --init_byte_ver <= init_byte;
     sclk <= sclk_internal;
-    --data_recieved_ver <= data_recieved;
-    ce <= ce_internal;
 
    
     ----------- SLOWER CLOCK USED TO SYNC DATA TRANSFER -----------
@@ -79,38 +73,28 @@ entity rtc is
     ----------- PROCESS -----------
     state_next_proc : process (sclk_internal, current_state, init_byte)
     begin
-        if current_state = idle then
-          ce_internal <= '0';
-          send <= '0';
-          --state <= "001"; -- For verification
-          next_state <= transmit;
-        elsif current_state = transmit then        
-          ce_internal <= '1';
-          send <= '1';
-          --state <= "010"; -- For verification
-          if init_byte = "00000000" then
-            next_state <= recieve;
-          end if;
-        elsif current_state = recieve then
-          ce_internal <= '1';
-          send <= '0';
-          --state <= "011"; -- For verification
+        if rising_edge(sclk_internal) then
+            if current_state = idle then
+              init_byte <= "10000011"; -- Reloads the initial data
+              --state <= "001";
+              data_trans <= 'Z';
+              ce <= '0';
+              next_state <= transmit;
+            elsif current_state = transmit then
+              ce <= '1';
+              --state <= "010";
+              data_trans <= init_byte(0);
+              init_byte <= '0' & init_byte(7 downto 1);
+              if init_byte = "00000000" then
+                next_state <= recieve;
+              end if;
+            elsif current_state = recieve then
+              ce <= '1';
+              --state <= "011";
+              data_trans <= 'Z';
+            else
+              next_state <= idle;
+            end if;
         end if;
     end process;
-
-
-    trans_proc : process (sclk_internal, send)
-    begin
-      if send = '0' then
-        init_byte <= "10000011"; -- Reloads the initial data
-        data_trans <= 'Z';
-        if ce_internal = '1' then
-          data_recieved <= data_trans & data_recieved(7 downto 1);
-        end if;
-      elsif send = '1' then
-        data_trans <= init_byte(0);
-        init_byte <= '0' & init_byte(7 downto 1);
-      end if;
-    end process;
-
 end architecture;
